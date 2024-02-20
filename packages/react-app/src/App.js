5fcb301a1dc71522759cd0e1acc8560b7a8a884a
Josephtran102 <josephtran0505@gmail.com>
2024-02-20 12:38:44 +0700

    Commit message

---
+++ packages/react-app/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useEthers } from "@usedapp/core";
+
+import styles from "./styles";
+import { usePools } from "./hooks";
+import { uniswapLogo } from "./assets";
+import { Exchange, Loader, WalletButton } from "./components";
+
+const App = () => {
+  const { account } = useEthers();
+  const [poolsLoading, pools] = usePools();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img
+            src={uniswapLogo}
+            alt="uniswap-logo"
+            className="w-56 h-16 object-contain"
+          />
+          <WalletButton />
+        </header>
+
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Joseph Tran Swap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient" />
+              <div className={styles.exchange}>
+                {account ? (
+                  poolsLoading ? (
+                    <Loader title="Loading pools, please wait!" />
+                  ) : (
+                    <Exchange pools={pools} />
+                  )
+                ) : (
+                  <Loader title="Please connect your wallet" />
+                )}
+              </div>
+              <div className="blue_gradient" />
+            </div>
+          </div>
+        </div>
+
+        {/* Phần footer */}
+        <footer className={styles.footer}>
+          <p className={styles.footerText}>Created by @Joseph Tran</p>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default App;
